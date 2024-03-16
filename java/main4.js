7a0cfce7e84cfc6aa4cdaaab9d2da3ae145d3e9f
paul_madaras <Madaras.Fl.Paul@student.utcluj.ro>
2024-03-16 14:10:23 +0200

    tried a new interface

---
+++ java/main4.js
@@ -0,0 +1,17 @@
+﻿function check(){
+	var q1 = quiz.q1.value;
+	var q2 = quiz.q2.value;
+	var q3 = quiz.q3.value;
+	var correct = 0;
+	
+	if(q1 == "C"){
+		correct++;
+}
+	if(q2 == "C"){
+		correct++;
+}
+	if(q3 == "D"){
+		correct++;
+}
+	alert("Ai răspuns corect la " + correct + "/3 întrebari.");
+}
